e77aa8adfe17b32181961dcf0bcbbf563316a724
naok1 <puni87gou@gmail.com>
2026-07-26 18:12:24 +0900

    refactor: point bosonic foundations umbrella

--- LeanCondensedMatter/SecondQuantization/Bosonic/Foundations.lean
+++ LeanCondensedMatter/SecondQuantization/Bosonic/Foundations.lean
@@ -1,4 +1,4 @@
-import LeanCondensedMatter.SecondQuantization.Bosonic.FockSpace
+import LeanCondensedMatter.SecondQuantization.Bosonic.Foundations.FockSpace
 
 set_option linter.style.header false
 
