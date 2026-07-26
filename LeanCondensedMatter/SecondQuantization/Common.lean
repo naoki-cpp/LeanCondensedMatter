c4392411ff51891d05a7d5cf142d3331bc233faf
naok1 <puni87gou@gmail.com>
2026-07-27 01:19:48 +0900

    refactor: extract common component restriction (#208)
    * refactor: extract common component restriction
    
    * refactor: preserve fermionic restriction api
    
    * refactor: export common component restriction
    
    * chore: target component restriction ci
    
    * fix: preserve fermionic leg membership definition
    
    * chore: restore full lean ci

--- LeanCondensedMatter/SecondQuantization/Common.lean
+++ LeanCondensedMatter/SecondQuantization/Common.lean
@@ -15,6 +15,7 @@ import LeanCondensedMatter.SecondQuantization.Common.QuarticLeg
 import LeanCondensedMatter.SecondQuantization.Common.QuarticDiagram
 import LeanCondensedMatter.SecondQuantization.Common.QuarticDiagramConnected
 import LeanCondensedMatter.SecondQuantization.Common.QuarticDiagramComponentPartition
+import LeanCondensedMatter.SecondQuantization.Common.QuarticDiagramComponentRestriction
 import LeanCondensedMatter.SecondQuantization.Common.BlochDeDominicis
 
 set_option linter.style.header false
