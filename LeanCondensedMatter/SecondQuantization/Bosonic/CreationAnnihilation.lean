f73cc7979d17c32315f5b93b30b5d28a397a6bee
naok1 <puni87gou@gmail.com>
2026-07-26 18:14:03 +0900

    refactor: use bosonic foundations path

--- LeanCondensedMatter/SecondQuantization/Bosonic/CreationAnnihilation.lean
+++ LeanCondensedMatter/SecondQuantization/Bosonic/CreationAnnihilation.lean
@@ -1,4 +1,4 @@
-import LeanCondensedMatter.SecondQuantization.Bosonic.FockSpace
+import LeanCondensedMatter.SecondQuantization.Bosonic.Foundations.FockSpace
 import Mathlib.Analysis.SpecialFunctions.Sqrt
 import Mathlib.LinearAlgebra.Finsupp.LSum
 
