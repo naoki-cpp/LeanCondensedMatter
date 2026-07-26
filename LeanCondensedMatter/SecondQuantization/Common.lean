5e814582bf02e3013f03818221d32eb8ca8dbe28
naok1 <puni87gou@gmail.com>
2026-07-26 16:07:23 +0900

    chore: export common quartic connectivity

--- LeanCondensedMatter/SecondQuantization/Common.lean
+++ LeanCondensedMatter/SecondQuantization/Common.lean
@@ -13,6 +13,7 @@ import LeanCondensedMatter.SecondQuantization.Common.WeightedDiagonalFunctional
 import LeanCondensedMatter.SecondQuantization.Common.KMSRotation
 import LeanCondensedMatter.SecondQuantization.Common.QuarticLeg
 import LeanCondensedMatter.SecondQuantization.Common.QuarticDiagram
+import LeanCondensedMatter.SecondQuantization.Common.QuarticDiagramConnected
 import LeanCondensedMatter.SecondQuantization.Common.BlochDeDominicis
 
 set_option linter.style.header false
