f6c17782a80913410c3b08a8a13ae773830cadb0
naok1 <puni87gou@gmail.com>
2026-07-27 11:08:59 +0900

    refactor: remove redundant bosonic foundations umbrella

--- LeanCondensedMatter/SecondQuantization/Bosonic/Foundations.lean
+++
@@ -1,9 +0,0 @@
-import LeanCondensedMatter.SecondQuantization.Bosonic.Foundations.FockSpace
-
-set_option linter.style.header false
-
-/-!
-# Bosonic foundations
-
-Occupation-number states and the algebraic bosonic Fock space.
--/
