f1b4c9b2ab28b20ffe870ae3bd2d844645dc9a95
naok1 <puni87gou@gmail.com>
2026-08-02 03:48:09 +0900

    Import diagonal spectral trace module

--- LeanCondensedMatter.lean
+++ LeanCondensedMatter.lean
@@ -16,6 +16,7 @@ import LeanCondensedMatter.Analysis.Operator.TraceClass.Basic
 import LeanCondensedMatter.Analysis.Operator.TraceClass.Bundled
 import LeanCondensedMatter.Analysis.Operator.TraceClass.Diagonal
 import LeanCondensedMatter.Analysis.Operator.TraceClass.DiagonalPositive
+import LeanCondensedMatter.Analysis.Operator.TraceClass.DiagonalSpectralTrace
 import LeanCondensedMatter.Analysis.Operator.TraceClass.Ops
 import LeanCondensedMatter.Analysis.Operator.TraceClass.Scalar
 import LeanCondensedMatter.Combinatorics
