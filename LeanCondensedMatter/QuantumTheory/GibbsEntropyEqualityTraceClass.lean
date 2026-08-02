2f0c52f80c6c600eaf27576af2fb1478729ae72f
naok1 <puni87gou@gmail.com>
2026-08-02 12:18:23 +0900

    Reuse common eigenbasis energy formula

--- LeanCondensedMatter/QuantumTheory/GibbsEntropyEqualityTraceClass.lean
+++ LeanCondensedMatter/QuantumTheory/GibbsEntropyEqualityTraceClass.lean
@@ -1,5 +1,5 @@
-import LeanCondensedMatter.QuantumTheory.GibbsEntropyTraceClass
 import LeanCondensedMatter.QuantumTheory.DiagonalDensityLemmasTraceClass
+import LeanCondensedMatter.QuantumTheory.DiagonalEnergyTraceClass
 
 /-!
 # Gibbs-state entropy equality via trace-class operators
@@ -62,26 +62,8 @@ theorem vonNeumannEntropy_gibbsState (Hop : Observable H) (β : ℝ)
     have htrace := entropyOpSpectralTraceClass_trace_eq_tsum_diagonal
       ρ bE.toHilbertBasis w (fun i => by simpa using hρbE i) hsEntropy
     simpa only [tsum_fintype] using htrace
-  have hEnergyDiag (i : Fin (Module.finrank ℂ H)) :
-      (inner ℂ (bE i) ((ρ.op ∘L Hop.1) (bE i)) : ℂ).re = w i * E i := by
-    change (inner ℂ (bE i)
-      ((ρ.op : H →ₗ[ℂ] H) ((Hop.1 : H →ₗ[ℂ] H) (bE i))) : ℂ).re = w i * E i
-    have hrho := congrArg (fun x : H => (ρ.op : H →ₗ[ℂ] H) x) (hEbE i)
-    rw [hrho, map_smul, hρbE i, smul_smul, inner_smul_right,
-      inner_self_eq_norm_sq_to_K, bE.norm_eq_one]
-    simp
-    ring
-  have hEnergy : energyExpValue ρ Hop = ∑ i, w i * E i := by
-    rw [energyExpValue_eq_re_linearMap_trace]
-    rw [LinearMap.trace_eq_sum_inner
-      ((ρ.op ∘L Hop.1 : H →L[ℂ] H) : H →ₗ[ℂ] H) bE]
-    calc
-      (∑ i, inner ℂ (bE i) ((ρ.op ∘L Hop.1) (bE i))).re =
-          ∑ i, (inner ℂ (bE i) ((ρ.op ∘L Hop.1) (bE i)) : ℂ).re := by
-        simpa only [Complex.reCLM_apply] using
-          (map_sum Complex.reCLM
-            (fun i => inner ℂ (bE i) ((ρ.op ∘L Hop.1) (bE i))) Finset.univ)
-      _ = ∑ i, w i * E i := Finset.sum_congr rfl fun i _ => hEnergyDiag i
+  have hEnergy : energyExpValue ρ Hop = ∑ i, w i * E i :=
+    energyExpValue_eq_sum_common_eigenbasis ρ Hop bE w E hρbE hEbE
   have hEntropyTraceNonneg :
       0 ≤ (entropyOpSpectralTraceClass ρ hsEntropy).trace := by
     rw [hEntropyTrace]
