5754eae15fd37d94f5827bcf35de516a54b3924c
naok1 <puni87gou@gmail.com>
2026-08-07 23:41:50 +0900

    refactor(diagrammatics): migrate bosonic quartic leg import

--- LeanCondensedMatter/SecondQuantization/Bosonic/Diagrammatics/QuarticWickExpansion.lean
+++ LeanCondensedMatter/SecondQuantization/Bosonic/Diagrammatics/QuarticWickExpansion.lean
@@ -1,7 +1,7 @@
 import LeanCondensedMatter.Combinatorics.PerfectPairing.Evaluation
 import LeanCondensedMatter.SecondQuantization.Bosonic.Diagrammatics.QuarticLocalLeg
 import LeanCondensedMatter.SecondQuantization.Bosonic.Thermal.BlochDeDominicis.FreeExpectationRecursion
-import LeanCondensedMatter.SecondQuantization.Common.Diagrammatics.Leg
+import LeanCondensedMatter.SecondQuantization.Common.Diagrammatics.Quartic.Leg
 
 set_option linter.style.header false
 
