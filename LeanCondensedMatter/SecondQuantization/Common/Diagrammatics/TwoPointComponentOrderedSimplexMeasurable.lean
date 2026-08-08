d9c002e179c23de759c22769fb14b1b33673fc63
naok1 <puni87gou@gmail.com>
2026-08-08 10:04:48 +0900

    fix(lct): follow nested TwoPoint ordered-simplex module

--- LeanCondensedMatter/SecondQuantization/Common/Diagrammatics/TwoPointComponentOrderedSimplexMeasurable.lean
+++ LeanCondensedMatter/SecondQuantization/Common/Diagrammatics/TwoPointComponentOrderedSimplexMeasurable.lean
@@ -1,4 +1,4 @@
-import LeanCondensedMatter.SecondQuantization.Common.Diagrammatics.TwoPointComponentOrderedSimplex
+import LeanCondensedMatter.SecondQuantization.Common.Diagrammatics.TwoPoint.ComponentOrderedSimplex
 
 set_option linter.style.header false
 
