7fb5303784e0c9303ed024921b90be4fd587e204
naok1 <puni87gou@gmail.com>
2026-08-11 23:35:34 +0900

    refactor(qm): point evolution continuity at moved endpoint

--- LeanCondensedMatter/QuantumMechanics/SingleParticle/Continuum/SchrodingerEvolutionContinuity1D.lean
+++ LeanCondensedMatter/QuantumMechanics/SingleParticle/Continuum/SchrodingerEvolutionContinuity1D.lean
@@ -1,5 +1,5 @@
 import LeanCondensedMatter.QuantumMechanics.SingleParticle.Continuum.SchrodingerEvolutionProbability1D
-import LeanCondensedMatter.QuantumTheory.Continuum.SchrodingerContinuitySchwartzTotal1D
+import LeanCondensedMatter.QuantumMechanics.SingleParticle.Continuum.SchrodingerContinuitySchwartzTotal1D
 import Mathlib.Tactic
 
 set_option linter.style.header false
