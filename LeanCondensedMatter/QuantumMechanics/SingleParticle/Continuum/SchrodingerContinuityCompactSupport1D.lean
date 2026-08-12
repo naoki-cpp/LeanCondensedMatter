882400296a133746ca2f7eb43d30384603e6e56a
naok1 <puni87gou@gmail.com>
2026-08-13 02:30:58 +0900

    refactor(qm): relocate smeared continuity module

--- LeanCondensedMatter/QuantumMechanics/SingleParticle/Continuum/SchrodingerContinuityCompactSupport1D.lean
+++ LeanCondensedMatter/QuantumMechanics/SingleParticle/Continuum/SchrodingerContinuityCompactSupport1D.lean
@@ -1,4 +1,4 @@
-import LeanCondensedMatter.QuantumMechanics.SingleParticle.Continuum.SchrodingerContinuitySmeared1D
+import LeanCondensedMatter.QuantumMechanics.SingleParticle.Continuum.Continuity.Smeared1D
 import LeanCondensedMatter.Analysis.Calculus.WeakConservation1D
 import LeanCondensedMatter.QuantumMechanics.SingleParticle.Continuum.Probability.Integral1D
 import Mathlib.Analysis.Calculus.Deriv.Support
