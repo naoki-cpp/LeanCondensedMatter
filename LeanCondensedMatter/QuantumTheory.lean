c9b8f73fb4e540322a8511e7e7c07900ebe3b71f
naok1 <puni87gou@gmail.com>
2026-08-13 09:23:43 +0900

    feat(quantum): export pure-point Gibbs state

--- LeanCondensedMatter/QuantumTheory.lean
+++ LeanCondensedMatter/QuantumTheory.lean
@@ -33,6 +33,7 @@ import LeanCondensedMatter.QuantumTheory.Entropy.Basic
 import LeanCondensedMatter.QuantumTheory.Entropy.Diagonal
 import LeanCondensedMatter.QuantumTheory.FiniteDimensional.Entropy
 import LeanCondensedMatter.QuantumTheory.Gibbs.State
+import LeanCondensedMatter.QuantumTheory.Gibbs.PurePoint
 import LeanCondensedMatter.QuantumTheory.Gibbs.EnergyExpectation
 import LeanCondensedMatter.QuantumTheory.Gibbs.FreeEnergy
 import LeanCondensedMatter.QuantumTheory.Gibbs.Entropy
