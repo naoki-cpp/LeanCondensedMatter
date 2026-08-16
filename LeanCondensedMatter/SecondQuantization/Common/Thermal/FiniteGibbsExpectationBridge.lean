80838a5bbb1e061f1e71225d1070466872b30fc8
naok1 <puni87gou@gmail.com>
2026-08-16 19:51:40 +0900

    fix: import finite diagonal corollary from finite API

--- LeanCondensedMatter/SecondQuantization/Common/Thermal/FiniteGibbsExpectationBridge.lean
+++ LeanCondensedMatter/SecondQuantization/Common/Thermal/FiniteGibbsExpectationBridge.lean
@@ -1,6 +1,6 @@
 import LeanCondensedMatter.SecondQuantization.Common.Thermal.FiniteGibbsDensityOperator
 import LeanCondensedMatter.SecondQuantization.Common.Perturbation.FiniteOperatorIntegral
-import LeanCondensedMatter.QuantumTheory.DensityOperator.DiagonalFormula
+import LeanCondensedMatter.QuantumTheory.DensityOperator.Finite
 
 set_option linter.style.header false
 
