331a8f2e7566219c89ee2a938190d83aa529eca8
naok1 <puni87gou@gmail.com>
2026-08-19 22:43:23 +0900

    refactor(common): remove external connectivity forwarder (#1436)
    * refactor(common): import external connectivity from owner
    
    * refactor(common): import external connectivity from owner
    
    * refactor(common): remove external connectivity forwarder

--- LeanCondensedMatter/SecondQuantization/Common/Diagrammatics/TwoPoint/ComponentDecomposition.lean
+++ LeanCondensedMatter/SecondQuantization/Common/Diagrammatics/TwoPoint/ComponentDecomposition.lean
@@ -1,4 +1,4 @@
-import LeanCondensedMatter.SecondQuantization.Common.Diagrammatics.TwoPoint.ExternalConnectivity
+import LeanCondensedMatter.SecondQuantization.Common.Diagrammatics.TwoPoint.External.ExternalConnectivity
 
 set_option linter.style.header false
 
