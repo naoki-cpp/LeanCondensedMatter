412e36c03ec41a70c3b57bc86eca251fa2fab884
naok1 <puni87gou@gmail.com>
2026-08-20 12:06:10 +0900

    refactor(common): normalize quartic owner imports

--- LeanCondensedMatter/SecondQuantization/Common/Diagrammatics/Quartic/Factorization/ComponentEvaluation.lean
+++ LeanCondensedMatter/SecondQuantization/Common/Diagrammatics/Quartic/Factorization/ComponentEvaluation.lean
@@ -1,4 +1,4 @@
-import LeanCondensedMatter.SecondQuantization.Common.Diagrammatics.Quartic.ComponentGlobalCrossingParity
+import LeanCondensedMatter.SecondQuantization.Common.Diagrammatics.Quartic.Factorization.ComponentGlobalCrossingParity
 import LeanCondensedMatter.Combinatorics.PerfectPairing.Evaluation
 
 set_option linter.style.header false
