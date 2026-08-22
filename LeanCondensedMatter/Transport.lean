cac76aa473250662f3dc71b91a8d4e3c827f26a8
naok1 <puni87gou@gmail.com>
2026-08-22 21:28:12 +0900

    refactor(transport): remove linear-response bridge export

--- LeanCondensedMatter/Transport.lean
+++ LeanCondensedMatter/Transport.lean
@@ -3,7 +3,6 @@ import LeanCondensedMatter.Transport.ConductivityNormalization
 import LeanCondensedMatter.Transport.FiniteConductivityTable
 import LeanCondensedMatter.Transport.FiniteTrace
 import LeanCondensedMatter.Transport.System
-import LeanCondensedMatter.Transport.LinearResponse
 import LeanCondensedMatter.Transport.Resolvent
 import LeanCondensedMatter.Transport.ResolventSpectral
 import LeanCondensedMatter.Transport.FiniteKuboBastin
