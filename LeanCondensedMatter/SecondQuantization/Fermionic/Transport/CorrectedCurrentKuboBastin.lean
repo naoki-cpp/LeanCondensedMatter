72228dc305689a5135a6e265bb9169e77d03062b
naok1 <puni87gou@gmail.com>
2026-08-24 01:47:15 +0900

    fix(lean-4.33): disambiguate corrected current lattice namespace

--- LeanCondensedMatter/SecondQuantization/Fermionic/Transport/CorrectedCurrentKuboBastin.lean
+++ LeanCondensedMatter/SecondQuantization/Fermionic/Transport/CorrectedCurrentKuboBastin.lean
@@ -30,7 +30,7 @@ namespace SecondQuantization
 namespace Fermionic
 namespace Transport
 
-open Lattice
+open _root_.SecondQuantization.Fermionic.Lattice
 open QuantumTheory QuantumTheory.LinearResponse QuantumTheory.Transport
 
 noncomputable section
