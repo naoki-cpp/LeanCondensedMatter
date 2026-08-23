7dd435632ad08912432b9416906170744667626d
naok1 <puni87gou@gmail.com>
2026-08-23 12:08:19 +0900

    refactor(ahe): canonicalize Bastin pole-window import

--- LeanCondensedMatter/Transport/AnomalousHall/MassiveDirac/Bastin/PoleWindow.lean
+++ LeanCondensedMatter/Transport/AnomalousHall/MassiveDirac/Bastin/PoleWindow.lean
@@ -1,4 +1,4 @@
-import LeanCondensedMatter.Transport.AnomalousHall.MassiveDiracBastinPoleFactor
+import LeanCondensedMatter.Transport.AnomalousHall.MassiveDirac.Bastin.PoleFactor
 import Mathlib.Tactic
 
 set_option linter.style.header false
