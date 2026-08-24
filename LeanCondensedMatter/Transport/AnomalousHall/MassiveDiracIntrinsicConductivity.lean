ce92176503605f892c11b6ceec0344d93c50141b
naok1 <puni87gou@gmail.com>
2026-08-24 11:46:22 +0900

    refactor(ahe): retire model intrinsic streda shims

--- LeanCondensedMatter/Transport/AnomalousHall/MassiveDiracIntrinsicConductivity.lean
+++
@@ -1,5 +0,0 @@
-import LeanCondensedMatter.Transport.AnomalousHall.MassiveDirac.Intrinsic.Conductivity
-
-set_option linter.style.header false
-
-/-! Compatibility import. Canonical owner: `MassiveDirac.Intrinsic.Conductivity`. -/
