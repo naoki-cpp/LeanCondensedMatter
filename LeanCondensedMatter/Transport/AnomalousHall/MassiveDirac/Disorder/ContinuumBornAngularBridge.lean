a9bcb4038543388b536f53a4ebc928ed524e2dd6
naok1 <puni87gou@gmail.com>
2026-08-27 22:09:53 +0900

    fix(ahe): normalize casts before prefactor commutativity

--- LeanCondensedMatter/Transport/AnomalousHall/MassiveDirac/Disorder/ContinuumBornAngularBridge.lean
+++ LeanCondensedMatter/Transport/AnomalousHall/MassiveDirac/Disorder/ContinuumBornAngularBridge.lean
@@ -97,6 +97,7 @@ theorem finiteCutoffContinuumBornSelfEnergy_eq_polarIntegral
     (finiteCutoffContinuumBornGreenIntegral side v m probeEnergy broadening pMax)]
   simp only [RCLike.algebraMap_eq_ofReal, smul_smul]
   congr 1
+  push_cast
   ac_rfl
 
 end
