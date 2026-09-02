841500cd98d9300907b3d231ec60b2c7b086f15e
naok1 <puni87gou@gmail.com>
2026-09-02 16:01:38 +0900

    refactor(transport): move MassiveDirac channel broadening owner

--- LeanCondensedMatter/Transport/AnomalousHall/MassiveDirac/Disorder/SelfEnergyBroadeningLimit.lean
+++ LeanCondensedMatter/Transport/AnomalousHall/MassiveDirac/Disorder/SelfEnergyBroadeningLimit.lean
@@ -1,4 +1,4 @@
-import LeanCondensedMatter.Transport.AnomalousHall.MassiveDirac.Disorder.ChannelBroadeningLimit
+import LeanCondensedMatter.Transport.Models.MassiveDirac.Disorder.ChannelBroadeningLimit
 import Mathlib.Tactic
 
 set_option linter.style.header false
