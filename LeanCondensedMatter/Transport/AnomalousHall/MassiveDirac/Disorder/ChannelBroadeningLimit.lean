9ba517116ee5419eb3b13e3aec72486bc6d6fa65
naok1 <puni87gou@gmail.com>
2026-09-02 12:36:37 +0900

    refactor(transport): route channel broadening through model owner

--- LeanCondensedMatter/Transport/AnomalousHall/MassiveDirac/Disorder/ChannelBroadeningLimit.lean
+++ LeanCondensedMatter/Transport/AnomalousHall/MassiveDirac/Disorder/ChannelBroadeningLimit.lean
@@ -1,4 +1,4 @@
-import LeanCondensedMatter.Transport.AnomalousHall.MassiveDirac.Disorder.DenominatorBroadeningLimit
+import LeanCondensedMatter.Transport.Models.MassiveDirac.Disorder.DenominatorBroadeningLimit
 import Mathlib.Tactic
 
 set_option linter.style.header false
