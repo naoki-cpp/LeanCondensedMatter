ea8a65ed9c7a56102d87b1c5230a4dcbd0d6c6f2
naok1 <puni87gou@gmail.com>
2026-09-05 10:45:39 +0900

    refactor(transport): move MassiveDirac Bastin radial spectator bound (#1976)
    * refactor(transport): move MassiveDirac Bastin radial spectator bound
    
    * refactor(transport): update radial spectator uniform import
    
    * refactor(transport): update Bastin radial spectator bound import
    
    * refactor(architecture): track model-owned Bastin radial spectator bound

--- LeanCondensedMatter/Transport/Models/MassiveDirac/Bastin/RadialSpectatorUniformBound.lean
+++ LeanCondensedMatter/Transport/Models/MassiveDirac/Bastin/RadialSpectatorUniformBound.lean
@@ -1,4 +1,4 @@
-import LeanCondensedMatter.Transport.AnomalousHall.MassiveDirac.Bastin.RadialSpectatorBound
+import LeanCondensedMatter.Transport.Models.MassiveDirac.Bastin.RadialSpectatorBound
 import Mathlib.Tactic
 
 set_option linter.style.header false
