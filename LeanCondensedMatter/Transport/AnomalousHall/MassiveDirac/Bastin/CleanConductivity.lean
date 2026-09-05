9c3982d96412bdf6b0a5f69070d28d516ab2b35f
naok1 <puni87gou@gmail.com>
2026-09-05 10:50:02 +0900

    refactor(transport): update clean conductivity PairBerry import

--- LeanCondensedMatter/Transport/AnomalousHall/MassiveDirac/Bastin/CleanConductivity.lean
+++ LeanCondensedMatter/Transport/AnomalousHall/MassiveDirac/Bastin/CleanConductivity.lean
@@ -1,4 +1,4 @@
-import LeanCondensedMatter.Transport.AnomalousHall.MassiveDirac.Bastin.PairBerry
+import LeanCondensedMatter.Transport.Models.MassiveDirac.Bastin.PairBerry
 import LeanCondensedMatter.Transport.AnomalousHall.MassiveDirac.Intrinsic.Conductivity
 import Mathlib.Tactic
 
