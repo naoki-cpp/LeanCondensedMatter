5268d9318c24f15f19bf59fa531c71c95ac86495
naok1 <puni87gou@gmail.com>
2026-09-06 12:18:45 +0900

    refactor(transport): redirect Bastin to Berry occupation

--- LeanCondensedMatter/Transport/Models/MassiveDirac/Bastin/CleanLimit.lean
+++ LeanCondensedMatter/Transport/Models/MassiveDirac/Bastin/CleanLimit.lean
@@ -1,5 +1,5 @@
 import LeanCondensedMatter.Transport.Models.MassiveDirac.Bastin.PairBerry
-import LeanCondensedMatter.Transport.Models.MassiveDirac.Conductivity.Hall.Intrinsic.Response
+import LeanCondensedMatter.Transport.Models.MassiveDirac.Model.Berry.Occupation
 import Mathlib.Tactic
 
 set_option linter.style.header false
