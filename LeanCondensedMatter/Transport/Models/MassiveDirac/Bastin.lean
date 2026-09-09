e73b151d99f005407111de759e2c0b63d4d793af
naok1 <puni87gou@gmail.com>
2026-09-09 11:10:57 +0900

    refactor(bastin): drop pole window bound module export

--- LeanCondensedMatter/Transport/Models/MassiveDirac/Bastin.lean
+++ LeanCondensedMatter/Transport/Models/MassiveDirac/Bastin.lean
@@ -6,7 +6,6 @@ import LeanCondensedMatter.Transport.Models.MassiveDirac.Bastin.Interband
 import LeanCondensedMatter.Transport.Models.MassiveDirac.Bastin.PoleFactor
 import LeanCondensedMatter.Transport.Models.MassiveDirac.Bastin.PoleWindow
 import LeanCondensedMatter.Transport.Models.MassiveDirac.Bastin.PoleContinuity
-import LeanCondensedMatter.Transport.Models.MassiveDirac.Bastin.PoleWindowBound
 import LeanCondensedMatter.Transport.Models.MassiveDirac.Bastin.PoleExtraction
 import LeanCondensedMatter.Transport.Models.MassiveDirac.Bastin.PairIntegral
 import LeanCondensedMatter.Transport.Models.MassiveDirac.Bastin.PairBerry
