f3d105c6a09fac737fe4ce06dfde891c7a68fba2
naok1 <puni87gou@gmail.com>
2026-09-10 03:30:48 +0900

    fix(massivedirac): remove redundant tactics

--- LeanCondensedMatter/Transport/Models/MassiveDirac/Model/Operator.lean
+++ LeanCondensedMatter/Transport/Models/MassiveDirac/Model/Operator.lean
@@ -134,9 +134,7 @@ theorem current_isHermitian (direction : Direction2) (e v : ℝ) :
     have hcurrent :
         current .x e v =
           InternalSpace.pauliCombination (fun axis => (u axis : ℂ)) := by
-      simp [current, velocity, directionPauli, InternalSpace.pauliCombination, u, smul_smul] <;>
-        push_cast <;>
-        module
+      simp [current, velocity, directionPauli, InternalSpace.pauliCombination, u, smul_smul]
     rw [hcurrent]
     exact InternalSpace.pauliCombination_ofReal_isHermitian u
   · let u : PauliAxis → ℝ
@@ -146,9 +144,7 @@ theorem current_isHermitian (direction : Direction2) (e v : ℝ) :
     have hcurrent :
         current .y e v =
           InternalSpace.pauliCombination (fun axis => (u axis : ℂ)) := by
-      simp [current, velocity, directionPauli, InternalSpace.pauliCombination, u, smul_smul] <;>
-        push_cast <;>
-        module
+      simp [current, velocity, directionPauli, InternalSpace.pauliCombination, u, smul_smul]
     rw [hcurrent]
     exact InternalSpace.pauliCombination_ofReal_isHermitian u
 
