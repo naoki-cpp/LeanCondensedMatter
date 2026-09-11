04fd478ff8d91109ac4a0ce8351e901d47385ebe
naok1 <puni87gou@gmail.com>
2026-09-11 12:00:57 +0900

    fix(crystal): import subgroup definitions

--- LeanCondensedMatter/Crystal/Symmetry.lean
+++ LeanCondensedMatter/Crystal/Symmetry.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Naoki Yano
 -/
 import LeanCondensedMatter.Crystal.AtomicConfiguration
+import Mathlib.Algebra.Group.Subgroup.Defs
 import Mathlib.Topology.MetricSpace.Isometry
 
 /-!
