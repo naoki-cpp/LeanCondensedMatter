e155d8dd42cd33ecda48aeeda2b1f9da223bec8f
naok1 <puni87gou@gmail.com>
2026-09-12 00:07:56 +0900

    feat(crystal): export lattice geometry

--- LeanCondensedMatter/Crystal.lean
+++ LeanCondensedMatter/Crystal.lean
@@ -1,3 +1,4 @@
 import LeanCondensedMatter.Crystal.AtomicConfiguration
 import LeanCondensedMatter.Crystal.Symmetry
 import LeanCondensedMatter.Crystal.Translation
+import LeanCondensedMatter.Crystal.Lattice
