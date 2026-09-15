584fb765ea8792d6043cf9f5d4fb0f03558965f7
naok1 <puni87gou@gmail.com>
2026-09-15 23:00:43 +0900

    refactor(fermionic): import finite-Hilbert owner directly

--- LeanCondensedMatter/SecondQuantization/Fermionic/Lattice/Bounded.lean
+++ LeanCondensedMatter/SecondQuantization/Fermionic/Lattice/Bounded.lean
@@ -1,6 +1,6 @@
 import LeanCondensedMatter.SecondQuantization.Fermionic.Lattice.Peierls
 import LeanCondensedMatter.SecondQuantization.Fermionic.Algebra.AlgebraicFock.OccupationEquivalence
-import LeanCondensedMatter.SecondQuantization.Common.Algebra.FiniteHilbertOperatorAlgebra
+import LeanCondensedMatter.SecondQuantization.Common.Algebra.FiniteHilbertOperator
 
 set_option linter.style.header false
 
