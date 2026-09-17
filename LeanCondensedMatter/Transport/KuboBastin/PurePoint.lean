1c859403e35f3fd3d3300ebe15d160342c767153
naok1 <puni87gou@gmail.com>
2026-09-17 16:02:18 +0900

    refactor(kubo-bastin): consume canonical Lehmann weight

--- LeanCondensedMatter/Transport/KuboBastin/PurePoint.lean
+++ LeanCondensedMatter/Transport/KuboBastin/PurePoint.lean
@@ -140,7 +140,8 @@ theorem purePointLehmannVertexTerm_eq_bastinSpectral
     system.hbar omega eta (data.energy mn.1) (data.energy mn.2) hhbar]
   unfold purePointKuboBastinSpectralVertexTerm
   rw [hinner]
-  unfold purePointTransitionWeight
+  unfold purePointTransitionWeight purePointTransitionData
+    orderedLehmannTransitionData LehmannTransitionData.weight
   field_simp [hhbar, hhbarComplex, hshift]
 
 end
