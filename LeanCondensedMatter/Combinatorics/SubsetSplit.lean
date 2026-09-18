325dc9d1b9368df032c66f8c7668c330f6d0960b
naok1 <puni87gou@gmail.com>
2026-09-18 22:14:52 +0900

    refactor(combinatorics): use Mathlib subtype equivalence directly

--- LeanCondensedMatter/Combinatorics/SubsetSplit.lean
+++ LeanCondensedMatter/Combinatorics/SubsetSplit.lean
@@ -18,32 +18,6 @@ namespace Combinatorics
 
 variable {α : Type*} [DecidableEq α]
 
-
-/-- A subtype of elements of `S` that lie in the subset `T` is canonically equivalent to
-`↥T`. -/
-def subsetSubtypeEquiv {S : Finset α} (T : Finset α) (h : T ⊆ S) :
-    {x : ↥S // (x : α) ∈ T} ≃ ↥T where
-  toFun x := ⟨x.1.1, x.2⟩
-  invFun x := ⟨⟨x.1, h x.2⟩, x.2⟩
-  left_inv x := by
-    apply Subtype.ext
-    apply Subtype.ext
-    rfl
-  right_inv x := by
-    apply Subtype.ext
-    rfl
-
-@[simp]
-theorem subsetSubtypeEquiv_val {S : Finset α} (T : Finset α) (h : T ⊆ S)
-    (x : {x : ↥S // (x : α) ∈ T}) :
-    (subsetSubtypeEquiv T h x : α) = (x : α) :=
-  rfl
-
-@[simp]
-theorem subsetSubtypeEquiv_symm_val {S : Finset α} (T : Finset α) (h : T ⊆ S) (x : ↥T) :
-    (((subsetSubtypeEquiv T h).symm x : {x : ↥S // (x : α) ∈ T}) : α) = (x : α) :=
-  rfl
-
 /-- A finite set is its subset together with the relative complement. -/
 def subsetSumSdiffEquiv {S T : Finset α} (h : T ⊆ S) : ↥T ⊕ ↥(S \ T) ≃ ↥S where
   toFun := Sum.elim (fun x => ⟨x.1, h x.2⟩) (fun x => ⟨x.1, (Finset.mem_sdiff.mp x.2).1⟩)
