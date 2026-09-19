969113dbae412c065adf3709d6e4b4c04e60aa48
naok1 <puni87gou@gmail.com>
2026-09-19 14:05:15 +0900

    refactor(shuffle): canonicalize vacuum coordinates (#2483)

--- LeanCondensedMatter/SecondQuantization/Fermionic/Diagrammatics/TwoPointDiagramExpansion/Integration/FiberShuffleOrderedData.lean
+++ LeanCondensedMatter/SecondQuantization/Fermionic/Diagrammatics/TwoPointDiagramExpansion/Integration/FiberShuffleOrderedData.lean
@@ -85,8 +85,7 @@ noncomputable def fixedExternalShuffleVacuumOrderedDataEquiv {m k : ℕ}
     QuarticWickDiagram Mode (m + k)
         ((Finset.univ : Finset (Fin (m + k))) \ shuffle.leftSlots) ≃
       Common.OrderedQuarticDiagramData (QuarticVertexLabel Mode) k :=
-  (Common.quarticDiagramEquivOrderedData (slotSplitVacuumOrder shuffle.leftSlots)).trans
-    (Equiv.cast (by rw [shuffle.card_sdiff_leftSlots]))
+  Common.quarticDiagramEquivOrderedData shuffle.sdiffLeftSlotsOrderEquiv
 
 /-- A fixed-cardinality fiber has shuffle-independent local data: one connected order-`m` external
 diagram and one order-`k` vacuum label/pairing datum. -/
