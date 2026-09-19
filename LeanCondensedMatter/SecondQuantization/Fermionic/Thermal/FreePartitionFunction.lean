e1a374212ab43f600b644b9cc279df14841f8b96
naok1 <puni87gou@gmail.com>
2026-09-19 12:01:24 +0900

    refactor(thermal): use finite Gibbs coordinate owner

--- LeanCondensedMatter/SecondQuantization/Fermionic/Thermal/FreePartitionFunction.lean
+++ LeanCondensedMatter/SecondQuantization/Fermionic/Thermal/FreePartitionFunction.lean
@@ -1,7 +1,7 @@
 import LeanCondensedMatter.SecondQuantization.Fermionic.Thermal.FreeBoltzmannCore
 import LeanCondensedMatter.SecondQuantization.Fermionic.Thermal.FreeGibbsDensityOperator
 import LeanCondensedMatter.SecondQuantization.Fermionic.Algebra.WeightedNumberOperator
-import LeanCondensedMatter.SecondQuantization.Common.Thermal.BlochDeDominicis.GibbsExpectation.Core
+import LeanCondensedMatter.SecondQuantization.Common.Thermal.FiniteGibbsCoordinate
 
 set_option linter.style.header false
 
