8bf7256051a8bb5e254ca80d3bca649f1dc49cfb
naok1 <puni87gou@gmail.com>
2026-09-20 22:12:03 +0900

    refactor(transport): make continuum measure imports explicit

--- LeanCondensedMatter/Transport/Models/MassiveDirac/Streda/GaussianCrossedRealSpaceIntegral.lean
+++ LeanCondensedMatter/Transport/Models/MassiveDirac/Streda/GaussianCrossedRealSpaceIntegral.lean
@@ -1,3 +1,4 @@
+import LeanCondensedMatter.Transport.Analysis.ContinuumMeasure
 import LeanCondensedMatter.Transport.Models.MassiveDirac.Streda.GaussianCrossedRealSpace
 import Mathlib.MeasureTheory.Integral.IntervalIntegral.Basic
 
