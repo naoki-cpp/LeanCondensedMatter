728630fb1cf77fa2a4fe0ee8503cc6cb07f6450f
naok1 <puni87gou@gmail.com>
2026-09-23 07:57:46 +0900

    refactor(cumulant): keep replica bridge out of combinatorics routing

--- LeanCondensedMatter/Combinatorics/Cumulant.lean
+++ LeanCondensedMatter/Combinatorics/Cumulant.lean
@@ -1,6 +1,5 @@
 import LeanCondensedMatter.Combinatorics.Cumulant.Moment
 import LeanCondensedMatter.Combinatorics.Cumulant.Replica
-import LeanCondensedMatter.Combinatorics.Cumulant.ReplicaBridge
 import LeanCondensedMatter.Combinatorics.Cumulant.Inversion
 import LeanCondensedMatter.Combinatorics.Cumulant.Normalized
 import LeanCondensedMatter.Combinatorics.Cumulant.Independence
