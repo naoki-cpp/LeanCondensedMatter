22af0d2216619e36d30f8cf3cdcf70558c6c2ed0
naok1 <puni87gou@gmail.com>
2026-09-23 12:47:27 +0900

    refactor(perfect-pairing): migrate relabel consumers to transport

--- LeanCondensedMatter/SecondQuantization/Common/Diagrammatics/Quartic/Components/Reassemble.lean
+++ LeanCondensedMatter/SecondQuantization/Common/Diagrammatics/Quartic/Components/Reassemble.lean
@@ -1,4 +1,4 @@
-import LeanCondensedMatter.Combinatorics.PerfectPairing.Relabel
+import LeanCondensedMatter.Combinatorics.PerfectPairing.Transport
 import LeanCondensedMatter.SecondQuantization.Common.Diagrammatics.Quartic.Components.ComponentConnected
 
 set_option linter.style.header false
