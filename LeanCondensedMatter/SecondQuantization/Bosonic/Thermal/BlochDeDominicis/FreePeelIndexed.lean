ed55d0f90538b485f18964cc1a2b23f2df2edb83
naok1 <puni87gou@gmail.com>
2026-09-25 10:50:04 +0900

    refactor(bosonic): follow generic exchange peel API

--- LeanCondensedMatter/SecondQuantization/Bosonic/Thermal/BlochDeDominicis/FreePeelIndexed.lean
+++ LeanCondensedMatter/SecondQuantization/Bosonic/Thermal/BlochDeDominicis/FreePeelIndexed.lean
@@ -45,7 +45,7 @@ theorem operatorPeelSum_eq_operatorPeelTerms_sum
   induction l with
   | nil =>
       simp [FreeThermalField.operatorPeelSum, operatorPeelTerms,
-        LinearMap.operatorPeelSum]
+        ScalarExchange.operatorPeelSum]
   | cons D t ih =>
       have hmap :
           ((C₁.operatorPeelTerms t).map (fun A => D.operator.comp A)).sum =
@@ -54,7 +54,7 @@ theorem operatorPeelSum_eq_operatorPeelTerms_sum
           (map_list_sum (LinearMap.compRight ℂ D.operator) (C₁.operatorPeelTerms t)).symm
       rw [operatorPeelTerms, List.sum_cons, hmap, ← ih]
       unfold FreeThermalField.operatorPeelSum
-      rw [LinearMap.operatorPeelSum]
+      rw [ScalarExchange.operatorPeelSum]
       simp only [one_smul]
       rw [orderedProduct]
 
